4c4e76b642e2a1de443751d336e3f0bbd2072bc4
Yulia Bondarenko <ybondarenko96@gmail.com>
2020-10-14 14:01:40 +0300

    added toolbar

---
+++ build/app/utext_autogen/Forms/ui_mainwindow.h
@@ -0,0 +1,176 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.9.7
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QSpacerItem>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QAction *actionsave;
+    QAction *actionactionundo;
+    QAction *actionredo;
+    QAction *actionfind;
+    QWidget *centralwidget;
+    QHBoxLayout *horizontalLayout_3;
+    QHBoxLayout *horizontalLayout_2;
+    QVBoxLayout *verticalLayout_3;
+    QHBoxLayout *horizontalLayout;
+    QLabel *label;
+    QLabel *label_2;
+    QSpacerItem *horizontalSpacer;
+    QHBoxLayout *horizontalLayout_4;
+    QMenuBar *menubar;
+    QToolBar *toolBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QStringLiteral("MainWindow"));
+        MainWindow->resize(969, 642);
+        QSizePolicy sizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(MainWindow->sizePolicy().hasHeightForWidth());
+        MainWindow->setSizePolicy(sizePolicy);
+        actionsave = new QAction(MainWindow);
+        actionsave->setObjectName(QStringLiteral("actionsave"));
+        QIcon icon;
+        icon.addFile(QStringLiteral(":/save.png"), QSize(), QIcon::Normal, QIcon::Off);
+        actionsave->setIcon(icon);
+        actionactionundo = new QAction(MainWindow);
+        actionactionundo->setObjectName(QStringLiteral("actionactionundo"));
+        QIcon icon1;
+        icon1.addFile(QStringLiteral(":/undo.png"), QSize(), QIcon::Normal, QIcon::Off);
+        actionactionundo->setIcon(icon1);
+        actionredo = new QAction(MainWindow);
+        actionredo->setObjectName(QStringLiteral("actionredo"));
+        QIcon icon2;
+        icon2.addFile(QStringLiteral(":/redo.png"), QSize(), QIcon::Normal, QIcon::Off);
+        actionredo->setIcon(icon2);
+        actionfind = new QAction(MainWindow);
+        actionfind->setObjectName(QStringLiteral("actionfind"));
+        QIcon icon3;
+        icon3.addFile(QStringLiteral(":/find.png"), QSize(), QIcon::Normal, QIcon::Off);
+        actionfind->setIcon(icon3);
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QStringLiteral("centralwidget"));
+        horizontalLayout_3 = new QHBoxLayout(centralwidget);
+        horizontalLayout_3->setObjectName(QStringLiteral("horizontalLayout_3"));
+        horizontalLayout_2 = new QHBoxLayout();
+        horizontalLayout_2->setObjectName(QStringLiteral("horizontalLayout_2"));
+        verticalLayout_3 = new QVBoxLayout();
+        verticalLayout_3->setObjectName(QStringLiteral("verticalLayout_3"));
+        horizontalLayout = new QHBoxLayout();
+#ifndef Q_OS_MAC
+        horizontalLayout->setSpacing(-1);
+#endif
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        horizontalLayout->setSizeConstraint(QLayout::SetMinimumSize);
+        label = new QLabel(centralwidget);
+        label->setObjectName(QStringLiteral("label"));
+        sizePolicy.setHeightForWidth(label->sizePolicy().hasHeightForWidth());
+        label->setSizePolicy(sizePolicy);
+        label->setMaximumSize(QSize(130, 60));
+        label->setAlignment(Qt::AlignCenter);
+
+        horizontalLayout->addWidget(label);
+
+        label_2 = new QLabel(centralwidget);
+        label_2->setObjectName(QStringLiteral("label_2"));
+        QSizePolicy sizePolicy1(QSizePolicy::Maximum, QSizePolicy::Preferred);
+        sizePolicy1.setHorizontalStretch(0);
+        sizePolicy1.setVerticalStretch(0);
+        sizePolicy1.setHeightForWidth(label_2->sizePolicy().hasHeightForWidth());
+        label_2->setSizePolicy(sizePolicy1);
+        label_2->setMinimumSize(QSize(0, 0));
+        label_2->setMaximumSize(QSize(80, 60));
+        label_2->setScaledContents(true);
+        label_2->setAlignment(Qt::AlignCenter);
+
+        horizontalLayout->addWidget(label_2);
+
+        horizontalSpacer = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout->addItem(horizontalSpacer);
+
+
+        verticalLayout_3->addLayout(horizontalLayout);
+
+        horizontalLayout_4 = new QHBoxLayout();
+        horizontalLayout_4->setSpacing(2);
+        horizontalLayout_4->setObjectName(QStringLiteral("horizontalLayout_4"));
+
+        verticalLayout_3->addLayout(horizontalLayout_4);
+
+
+        horizontalLayout_2->addLayout(verticalLayout_3);
+
+
+        horizontalLayout_3->addLayout(horizontalLayout_2);
+
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName(QStringLiteral("menubar"));
+        menubar->setGeometry(QRect(0, 0, 969, 22));
+        MainWindow->setMenuBar(menubar);
+        toolBar = new QToolBar(MainWindow);
+        toolBar->setObjectName(QStringLiteral("toolBar"));
+        toolBar->setMovable(false);
+        toolBar->setAllowedAreas(Qt::LeftToolBarArea);
+        toolBar->setOrientation(Qt::Vertical);
+        toolBar->setFloatable(false);
+        MainWindow->addToolBar(Qt::LeftToolBarArea, toolBar);
+
+        toolBar->addAction(actionsave);
+        toolBar->addAction(actionactionundo);
+        toolBar->addAction(actionredo);
+        toolBar->addAction(actionfind);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", Q_NULLPTR));
+        actionsave->setText(QApplication::translate("MainWindow", "save", Q_NULLPTR));
+        actionactionundo->setText(QApplication::translate("MainWindow", "actionundo", Q_NULLPTR));
+        actionredo->setText(QApplication::translate("MainWindow", "redo", Q_NULLPTR));
+        actionfind->setText(QApplication::translate("MainWindow", "find", Q_NULLPTR));
+        label->setText(QApplication::translate("MainWindow", "ucode", Q_NULLPTR));
+        label_2->setText(QApplication::translate("MainWindow", "text", Q_NULLPTR));
+        toolBar->setWindowTitle(QApplication::translate("MainWindow", "toolBar", Q_NULLPTR));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
